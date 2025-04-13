0f83f8e45bb5a6a0b4d1877d0d7ea6c8ba07833b
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-04-13 08:40:50 +0300

    Add night hours images

--- src/components/Forecats24Hours.jsx
+++ src/components/Forecats24Hours.jsx
@@ -2,32 +2,33 @@ import { getWeatherInfo } from "../utils/getWeatherInfo";
 
 export default function Forecast24Hours({ weatherForecast }) {
     const hourlyWeatherData = [...weatherForecast[0].hour, ...weatherForecast[1].hour];
-
-
     const now = Date.now() / 1000;
     const next24h = now + 24 * 60 * 60;
-    const next24hWeatherData = hourlyWeatherData.filter(item => 
+    const next24hWeatherData = hourlyWeatherData.filter(item =>
         item.time_epoch > now && item.time_epoch <= next24h
     )
 
-    
     const formatedHourlyWeatherInfo = next24hWeatherData.map(h => {
         return getWeatherInfo(h, 'forecast');
     });
 
-
-   
     return (
         <div className="forecast-container">
-            {formatedHourlyWeatherInfo.map((h, i) =>
-            (<div key={i} className="forecast-hourly-container">
-                <p>{h.time}</p>
-                <div className="hourly-img">
-                    <img src={`images/${h.weatherIcon}.svg`} alt="" />
-                </div>
-                <p>{h.temp}°</p>
-            </div>)
-            )}
+            <p>24h forecast</p>
+            <div className="hourly-weather-container">
+                {formatedHourlyWeatherInfo.map((h, i) =>
+                (<div key={i} className="each-hour-container">
+                    <p>{h.time}</p>
+                    <div className="hourly-img">
+                        {h.time > 6 && h.time < 20
+                            ? <img src={`images/${h.weatherIcon}.svg`} alt="" />
+                            : <img src={`images/${h.weatherIcon}-night.svg`} alt="" />
+                        }
+                    </div>
+                    <p>{h.temp}°</p>
+                </div>)
+                )}
+            </div>
         </div>
     )
 }
